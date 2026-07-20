509dc5b9c735eeb71b3347b1fca12f25870ca203
akifakif32 <mhmtkfdrn@gmail.com>
2026-07-20 20:29:45 +0300

    nee

--- extract-files.py
+++ extract-files.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
 #
-# SPDX-FileCopyrightText: The Infinity-X Project
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
 # SPDX-License-Identifier: Apache-2.0
 #
-
 from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
@@ -22,22 +22,18 @@ namespace_imports = [
     'hardware/qcom-caf/sm8250',
     'hardware/qcom-caf/wlan',
     'hardware/samsung',
+    'vendor/samsung/m23xq',
     'vendor/qcom/opensource/dataservices',
     'vendor/qcom/opensource/display',
 ]
 
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
 blob_fixups: blob_fixups_user_type = {
-    # Display / Scaler
-    (
-        'vendor/lib64/unihal_main@2.1.so',
-        'vendor/lib64/libscaler_hw.unifunc.so',
-        'vendor/lib/libscaler_hw.unifunc.so',
-    ): blob_fixup()
-        .add_needed('libui_shim.so'),
-    # DPPS
-    'vendor/lib64/libdpps.so': blob_fixup()
-        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
-    # Keymaster / Gatekeeper / DRM
+    'vendor/lib64/libsec-ril.so': blob_fixup()
+        .binary_regex_replace(b'ril.dds.call.ongoing', b'vendor.calls.ongoing'),
     (
         'vendor/lib64/hw/gatekeeper.mdfpp.so',
         'vendor/lib64/libskeymaster4device.so',
@@ -46,27 +42,30 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/lib/mediadrm/libwvdrmengine.so',
     ): blob_fixup()
         .replace_needed('libcrypto.so', 'libcrypto-v33.so'),
-    # Lights
+    (
+        'vendor/lib64/unihal_main@2.1.so',
+        'vendor/lib64/libscaler_hw.unifunc.so',
+        'vendor/lib/libscaler_hw.unifunc.so',
+    ): blob_fixup()
+        .add_needed('libui_shim.so'),
     (
         'vendor/lib64/vendor.samsung.hardware.light-V1-ndk_platform.so',
         'vendor/bin/hw/vendor.samsung.hardware.light-service',
     ): blob_fixup()
         .replace_needed('android.hardware.light-V1-ndk_platform.so', 'android.hardware.light-V1-ndk.so'),
-    # RIL
-    'vendor/lib64/libsec-ril.so': blob_fixup()
-        .binary_regex_replace(b'ril.dds.call.ongoing', b'vendor.calls.ongoing'),
-    # Wi-Fi
     'vendor/bin/hw/macloader': blob_fixup()
         .binary_regex_replace(b'vendor.wifi.dualconcurrent.interface', b'vnedor.wiff.dualconcurreut.iuterface')
         .binary_regex_replace(b'ro.vendor.wifi.sap.interface', b'ru.vnedor.wiff.sep.iuterface'),
+    'vendor/lib64/libdpps.so': blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
     'sm7225-common',
     'samsung',
-    namespace_imports=namespace_imports,
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
 )
 
 if __name__ == '__main__':
