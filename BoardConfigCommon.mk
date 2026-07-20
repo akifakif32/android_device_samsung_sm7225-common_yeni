fe547c50862201bbc40e217f70c64eced12b66a7
akifakif32 <mhmtkfdrn@gmail.com>
2026-07-20 21:49:50 +0300

    nee

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -203,7 +203,6 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 # SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 
 # Treble
-BOARD_VNDK_VERSION := current
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 
 # Wifi
