c2122b8c0cbc5e215b6e7d1ccad7a342ed5c0cf1
akifakif32 <mhmtkfdrn@gmail.com>
2026-07-20 16:35:02 +0300

    test

---
+++ extract-files.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2016 The CyanogenMod Project
+# SPDX-FileCopyrightText: 2017-2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+
+blob_fixups: blob_fixups_user_type = {
+    'vendor/lib64/libsec-ril.so': blob_fixup()
+        .binary_regex_replace(b'ril.dds.call.ongoing', b'vendor.calls.ongoing'),
+    (
+        'vendor/lib64/hw/gatekeeper.mdfpp.so',
+        'vendor/lib64/libskeymaster4device.so',
+        'vendor/lib64/libkeymaster_helper.so',
+        'vendor/lib/libwvhidl.so',
+        'vendor/lib/mediadrm/libwvdrmengine.so',
+    ): blob_fixup()
+        .replace_needed('libcrypto.so', 'libcrypto-v33.so'),
+    (
+        'vendor/lib64/unihal_main@2.1.so',
+        'vendor/lib64/libscaler_hw.unifunc.so',
+        'vendor/lib/libscaler_hw.unifunc.so',
+    ): blob_fixup()
+        .add_needed('libui_shim.so'),
+    (
+        'vendor/lib64/vendor.samsung.hardware.light-V1-ndk_platform.so',
+        'vendor/bin/hw/vendor.samsung.hardware.light-service',
+    ): blob_fixup()
+        .replace_needed('android.hardware.light-V1-ndk_platform.so', 'android.hardware.light-V1-ndk.so'),
+    'vendor/bin/hw/macloader': blob_fixup()
+        .binary_regex_replace(b'vendor.wifi.dualconcurrent.interface', b'vnedor.wiff.dualconcurreut.iuterface')
+        .binary_regex_replace(b'ro.vendor.wifi.sap.interface', b'ru.vnedor.wiff.sep.iuterface'),
+    'vendor/lib64/libdpps.so': blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'sm7225-common',
+    'samsung',
+    blob_fixups=blob_fixups,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()
